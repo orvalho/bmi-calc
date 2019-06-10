2567b0ee2ff71d8dad9fe23a1abde96f674f3509
orvalho <rasa396@gmail.com>
2019-06-11 01:01:43 +0300

    Updates

---
+++ precache-manifest.b0f8b67820d4e319a0c258032ed2a920.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6df7e394cbda334bf83694fbe4802925",
+    "url": "/bmi-calc/index.html"
+  },
+  {
+    "revision": "91f2640cbdd6f5bd6d2b",
+    "url": "/bmi-calc/static/css/main.45b261b7.chunk.css"
+  },
+  {
+    "revision": "8d36cca5275cefeeccd9",
+    "url": "/bmi-calc/static/js/2.eb502c2b.chunk.js"
+  },
+  {
+    "revision": "91f2640cbdd6f5bd6d2b",
+    "url": "/bmi-calc/static/js/main.a74b08cd.chunk.js"
+  },
+  {
+    "revision": "31ba27cfb24986980806",
+    "url": "/bmi-calc/static/js/runtime~main.4964525f.js"
+  }
+]);
